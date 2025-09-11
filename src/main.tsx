373444d4a1fe464ed71fc2fa62f9ac1952aae516
Pierino Hristov <pmglplus@gmail.com>
2025-09-11 17:23:13 +0100

    accept the changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const rootElement = document.getElementById('root');
-
 if (!rootElement) {
   throw new Error("Root element with id 'root' not found.");
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
